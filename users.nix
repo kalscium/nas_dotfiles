edafed36b6dc4c5894dd8de6347be3067e12bc7f
kalscium <kalscium@protonmail.com>
2024-12-12 23:40:31 +1100

    users: added more specific users
    I've changed the users to be more specific so I can administrate
    permissions better.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

--- users.nix
+++ users.nix
@@ -4,10 +4,17 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICesBj/2lEYFkkK27+d0xfO6MufVItX9w427gS5gIgFi root"
   ];
 
-  # Defines the general public user (set a password with passwd)
-  users.users."user" = {
+  # Defines my dad's user (set a password with passwd)
+  users.users."dad" = {
     isNormalUser = true;
-    description = "General User";
+    description = "Dad";
     extraGroups = [ "networkmanager" "uinput" ];
+  };
+
+  # Defines my user (set a password with passwd)
+  users.users."kalscium" = {
+    isNormalUser = true;
+    description = "The maintainer of the NAS";
+    extraGroups = [ "networkmanager" "uinput" "wheel" ];
   };
 }
