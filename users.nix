96e06e11f9473c0b6d9dfb3492d7680056f55ab7
kalscium <kalscium@protonmail.com>
2025-04-30 18:13:25 +1000

    root ssh key changes
    I've added my own ssh key to the root user aswell to prevent getting locked out of the system.
    And so the passwords on the system themselves are not known to me.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

--- users.nix
+++ users.nix
@@ -1,4 +1,8 @@
 {
+  users.users."root".openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICesBj/2lEYFkkK27+d0xfO6MufVItX9w427gS5gIgFi kalscium"
+  ];
+
   # Defines my user (set a password with passwd)
   users.users."server" = {
     isNormalUser = true;
